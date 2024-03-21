6b2c41551e4da00e37cf678b5509f75f42513d42
Silas Martins <silas.martins2041@gmail.com>
2024-03-21 09:05:08 -0300

    Update page.tsx - Atualizado

--- src/app/tips/6/page.tsx
+++ src/app/tips/6/page.tsx
@@ -2,6 +2,6 @@ import { Tip } from "@/components/Tip";
 
 export default function Tip4() {
   return (
-    <Tip name={6} tip1="Go down the stairs." tip2="Turn left and then right." tip3="Pass through the glass door." tip4="I'm here to remind you who founded me and when." senha="Box of chocolates." />
+    <Tip name={6} tip1="Go down the stairs." tip2="Turn left and then right." tip3="Pass through the glass door." tip4="I'm here to remind you who founded me and when." senha="Box of chocolate" />
   )
 }
