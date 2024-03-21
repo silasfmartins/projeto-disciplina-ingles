51a182de814d5feb42ad1073ae837b56670a1f72
Silas Martins <silas.martins2041@gmail.com>
2024-03-21 08:32:50 -0300

    Update Header.tsx - Nome da Equipe Novo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
   return (
     <header className="flex flex-col justify-between items-center p-8 py-1">
       <Image className="p-8 max-w-40 cursor-pointer" src={Logo} alt="Logo do Projeto de Inglês. É a logo do Navegador Brave" />
-      <h1 className="text-3xl sm:text-5xl font-bold">NOME EQUIPE</h1>
+      <h1 className="text-3xl sm:text-5xl font-bold uppercase">The beards</h1>
     </header>
   )
 }
