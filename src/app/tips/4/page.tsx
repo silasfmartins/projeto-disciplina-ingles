68866c6b8969807461a2436d2f358e0862855e14
Silas Martins <silas.martins2041@gmail.com>
2024-03-21 08:47:48 -0300

    Update page.tsx - Atualizado Dicas 4

--- src/app/tips/4/page.tsx
+++ src/app/tips/4/page.tsx
@@ -2,6 +2,6 @@ import { Tip } from "@/components/Tip";
 
 export default function Tip4() {
   return (
-    <Tip name={4} tip1="Climb four flights of stairs." tip2="Observe the surroundings." tip3="I am responsible for helping you navigate within the building." tip4="Hint: Where are you" senha="Second floor" />
+    <Tip name={4} tip1="Go towards the elevator." tip2="Facing the multimedia room." tip3="I was heavily used during the coronavirus pandemic." senha="Hand sanitizer" />
   )
 }
