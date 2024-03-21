845a187923f24d32075bb80fb796cd32fba9d1de
Silas Martins <silas.martins2041@gmail.com>
2024-03-21 08:53:15 -0300

    Update page.tsx - Update Tip5

--- src/app/tips/5/page.tsx
+++ src/app/tips/5/page.tsx
@@ -2,6 +2,6 @@ import { Tip } from "@/components/Tip";
 
 export default function Tip4() {
   return (
-    <Tip name={5} tip1="Return to the stairs." tip2="You seem thirsty?" tip3="Drink water." senha="Water fountain" />
+    <Tip name={5} tip1="Return to the stairs." tip2="You seem thirsty?" tip3="Drink water." senha="Drinking fountain." />
   )
 }
