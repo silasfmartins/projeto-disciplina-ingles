af035ff126c6f72e4d59490865fd2b921f64d623
Silas Martins <silas.martins2041@gmail.com>
2024-03-21 08:51:07 -0300

    Update page.tsx - Update Dica 5

--- src/app/tips/5/page.tsx
+++ src/app/tips/5/page.tsx
@@ -2,6 +2,6 @@ import { Tip } from "@/components/Tip";
 
 export default function Tip4() {
   return (
-    <Tip name={5} tip1="Climb four flights of stairs." tip2="Observe the surroundings." tip3="I am responsible for helping you navigate within the building." tip4="Hint: Where are you" senha="Second floor" />
+    <Tip name={5} tip1="Return to the stairs." tip2="You seem thirsty?" tip3="Drink water." senha="Water fountain" />
   )
 }
